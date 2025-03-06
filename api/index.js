ae5b38a4aff6142120eb68b7db8011ca98eb3572
Vaishali48 <vsakariya62@gmail.com>
2025-03-06 18:11:07 +0530

    finally

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ const app = express()
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL,'https://mern-blog-xnua.vercel.app'] ,
     credentials: true
 }))
 
